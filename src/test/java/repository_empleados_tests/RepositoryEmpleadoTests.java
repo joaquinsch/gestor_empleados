202b30ca9fea7488d1e24ecc9604042295f76599
joaquinsch <joacohao1@gmail.com>
2025-02-27 19:16:34 -0300

    test para buscar empleado por id

--- src/test/java/repository_empleados_tests/RepositoryEmpleadoTests.java
+++ src/test/java/repository_empleados_tests/RepositoryEmpleadoTests.java
@@ -7,8 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import repository.EmpleadoRepository;
+
+import java.util.Optional;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -21,6 +24,7 @@ public class RepositoryEmpleadoTests {
 
     @BeforeEach
     public void setUp(){
+        MockitoAnnotations.openMocks(this);
         empleado = new Empleado();
         empleado.setId_empleado(1L);
         empleado.setNombre("Juan");
@@ -36,5 +40,21 @@ public class RepositoryEmpleadoTests {
         Empleado empleadoGuardado = empleadoRepository.save(empleado);
         Mockito.verify(empleadoRepository, Mockito.times(1)).save(empleado);
         Assertions.assertNotNull(empleadoGuardado.getId_empleado());
+    }
+
+    @Test
+    public void deberiaEliminarEmpleadoPorId(){
+        empleadoRepository.deleteById(empleado.getId_empleado());
+        Mockito.verify(empleadoRepository).deleteById(empleado.getId_empleado());
+
+    }
+
+    @Test
+    public void deberiaBuscarEmpleadoPorId(){
+        Mockito.when(empleadoRepository.findById(empleado.getId_empleado())).thenReturn(Optional.of(empleado));
+        Optional<Empleado> empleadoBuscado = empleadoRepository.findById(empleado.getId_empleado());
+        Mockito.verify(empleadoRepository).findById(empleado.getId_empleado());
+        Assertions.assertEquals(empleadoBuscado.get().getId_empleado(), empleado.getId_empleado());
+
     }
 }
