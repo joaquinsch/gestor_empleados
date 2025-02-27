35637fdd42c867b96316a606c42a27e863a8bda2
joaquinsch <joacohao1@gmail.com>
2025-02-27 15:52:53 -0300

    refactor tests

--- src/test/java/empleado_tests/EmpleadoTests.java
+++ src/test/java/empleado_tests/EmpleadoTests.java
@@ -14,77 +14,79 @@ import java.util.Set;
 public class EmpleadoTests {
 
     private Validator validator;
+    private Empleado empleado;
+    private Set<ConstraintViolation<Empleado>> violations;
 
     @BeforeEach
     void setUp() {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
+
+        empleado = new Empleado(1L, "joaquín ernesto", "perez", 1000.0, "administrativo");
+
     }
 
     @Test
     public void deberiaCrearUnEmpleado(){
-        Empleado empleado = new Empleado();
-        Assertions.assertNotNull(empleado);
+        Empleado empleado2 = new Empleado();
+        Assertions.assertNotNull(empleado2);
     }
 
     @Test
     public void deberiaCrearseConId(){
-        Empleado empleado = new Empleado(1L, null, null, null,null);
+
         Assertions.assertEquals(1, empleado.getId_empleado());
     }
 
     @Test
     public void deberiaCrearseConUnNombre(){
-        Empleado empleado = new Empleado(1L, "joaquin",null,null,null);
         Assertions.assertEquals(1, empleado.getId_empleado());
-        Assertions.assertEquals("joaquin", empleado.getNombre());
+        Assertions.assertEquals("joaquín ernesto", empleado.getNombre());
     }
 
     @Test
     public void deberiaCrearseConUnApellido(){
-        Empleado empleado = new Empleado(1L, "joaquin","perez",null,null);
+
         Assertions.assertEquals("perez", empleado.getApellido());
     }
 
     @Test
     public void deberiaCrearseConUnSueldo(){
-        Empleado empleado = new Empleado(1L, "joaquin","perez", 1000.0,null);
+
         Assertions.assertEquals(1000, empleado.getSueldo());
     }
 
     @Test
     public void deberiaCrearseConUnPuesto(){
-        Empleado empleado = new Empleado(1L, "joaquin","perez", 1000.0, "administrativo");
+
         Assertions.assertEquals("administrativo", empleado.getPuesto());
     }
 
     @Test
     public void elNombreNoDeberiaContenerNumeros(){
-        Empleado empleado = new Empleado(1L, "joaquin123","perez", 1000.0, "administrativo");
-        Set<ConstraintViolation<Empleado>> violations = validator.validate(empleado);
+
+        Empleado empleado2 = new Empleado(1L, "joaquín1ernesto", "perez", 1000.0, "administrativo");
+        violations = validator.validate(empleado2);
+        Assertions.assertEquals("joaquín1ernesto", empleado2.getNombre());
         Assertions.assertFalse(violations.isEmpty(), "Se esperaba una violación de validación en el nombre");
         // verifica el mensaje de @Pattern
         Assertions.assertEquals("El nombre es inválido", violations.iterator().next().getMessage());
     }
     @Test
     public void deberiaCrearseSiElNombreEsCorrecto(){
-        Empleado empleado = new Empleado(1L, "joaquin", "perez", 1000.0, "administrativo");
-        Set<ConstraintViolation<Empleado>> violations = validator.validate(empleado);
-
+        violations = validator.validate(empleado);
         Assertions.assertTrue(violations.isEmpty(), "No se esperaba ninguna violación de validación");
     }
 
     @Test
     public void deberiaCrearseSiElNombreTieneEspacios(){
-        Empleado empleado = new Empleado(1L, "joaquin ernesto", "perez", 1000.0, "administrativo");
-        Set<ConstraintViolation<Empleado>> violations = validator.validate(empleado);
+        violations = validator.validate(empleado);
         Assertions.assertTrue(violations.isEmpty());
     }
 
     @Test
     public void deberiaCrearseSiNombreTieneAcentos(){
-        Empleado empleado = new Empleado(1L, "joaquín ernesto", "perez", 1000.0, "administrativo");
-        Set<ConstraintViolation<Empleado>> violations = validator.validate(empleado);
+        violations = validator.validate(empleado);
         Assertions.assertTrue(violations.isEmpty(), "no se esperaban violaciones");
     }
 }
